d7ed2dd70e6831c5cd6cceaa08974b65fcd0d916
hiroshi <hiroshi@looksrare.org>
2024-06-06 12:05:51 +0100

    build: Upgrade Solidity to 0.8.24

--- contracts/BlastTransferManager.sol
+++ contracts/BlastTransferManager.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.20;
+pragma solidity 0.8.24;
 
 import {TransferManager} from "./TransferManager.sol";
 import {IBlast, YieldMode, GasMode} from "./interfaces/IBlast.sol";
--- contracts/interfaces/IImmutableCreate2Factory.sol
+++ contracts/interfaces/IImmutableCreate2Factory.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.20;
+pragma solidity 0.8.24;
 
 interface IImmutableCreate2Factory {
     function hasBeenDeployed(address deploymentAddress) external returns (bool);

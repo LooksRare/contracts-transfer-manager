9b318c4271f18bc3309e897236724c5ac5463cd0
hiroshi <hiroshi@looksrare.org>
2024-06-06 11:53:32 +0100

    chore: Remove unused import

--- contracts/BlastTransferManager.sol
+++ contracts/BlastTransferManager.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.20;
 
-import {OwnableTwoSteps} from "@looksrare/contracts-libs/contracts/OwnableTwoSteps.sol";
 import {TransferManager} from "./TransferManager.sol";
 import {IBlast, YieldMode, GasMode} from "./interfaces/IBlast.sol";
 
